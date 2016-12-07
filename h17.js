d5fccc5736d9fa1b64f6a24615993f4259ce5ec1
phunziker <patrick@h17.ch>
2016-12-07 23:33:11 +0100

    Complete color change

--- h17.js
+++ h17.js
@@ -1,4 +1,4 @@
-t.prefs_.set('color-palette-overrides',["#ffffff", "#970b16", "#07962a", "#f8eec7", "#003e8a", "#e94691", "#89d1ec", "#ffffff", "#666666", "#de0000", "#87d5a2", "#f1d007", "#2e6cba", "#ffa29f", "#1cfafe", "#ffffff"]);
+t.prefs_.set('color-palette-overrides',["#ffffff", "#a82320", "#32a548", "#e58d11", "#3167ac", "#781aa0", "#2c9370", "#b0b6ba", "#212c3c", "#d4312e", "#2d9440", "#e5be0c", "#3c7dd2", "#8230a7", "#35b387", "#e7eced"]);
 t.prefs_.set('foreground-color', "#ffffff");
 t.prefs_.set('background-color', "#011f35");
 t.prefs_.set('cursor-color', "#ffffff");
