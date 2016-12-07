0ed121e9046eeea7a3a188a85bb82daa4a2b1ea8
phunziker <patrick@h17.ch>
2016-12-07 23:27:27 +0100

    Color change

--- h17.js
+++ h17.js
@@ -1,4 +1,4 @@
-t.prefs_.set('color-palette-overrides',["#3e3e3e", "#970b16", "#07962a", "#f8eec7", "#003e8a", "#e94691", "#89d1ec", "#ffffff", "#666666", "#de0000", "#87d5a2", "#f1d007", "#2e6cba", "#ffa29f", "#1cfafe", "#ffffff"]);
+t.prefs_.set('color-palette-overrides',["#ffffff", "#970b16", "#07962a", "#f8eec7", "#003e8a", "#e94691", "#89d1ec", "#ffffff", "#666666", "#de0000", "#87d5a2", "#f1d007", "#2e6cba", "#ffa29f", "#1cfafe", "#ffffff"]);
 t.prefs_.set('foreground-color', "#ffffff");
 t.prefs_.set('background-color', "#011f35");
 t.prefs_.set('cursor-color', "#ffffff");
